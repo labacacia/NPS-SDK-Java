ba8e41c882df267b04130f0bcb2314f49c630875
Claudie <claudie.dev@innolotus.com>
2026-05-12 05:45:00 +0000

    test: add JUnit Platform launcher

--- build.gradle.kts
+++ build.gradle.kts
@@ -41,6 +41,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
     testImplementation("org.mockito:mockito-core:5.12.0")
     testImplementation("org.mockito:mockito-junit-jupiter:5.12.0")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     testRuntimeOnly("org.slf4j:slf4j-simple:2.0.13")
 }
 
